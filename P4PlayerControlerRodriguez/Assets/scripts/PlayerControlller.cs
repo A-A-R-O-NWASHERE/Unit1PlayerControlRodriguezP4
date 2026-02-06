62f48d90864f9090fc0a681429a31fb9b0ef5049
A-A-R-O-NWASHERE <768335@student.dusd.net>
2026-02-06 11:35:42 -0800

    almost finished
    today i locked in and i nearly finish the player controller hopeful next time i would be able to finish it

--- P4PlayerControlerRodriguez/Assets/scripts/PlayerControlller.cs
+++ P4PlayerControlerRodriguez/Assets/scripts/PlayerControlller.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class PlayerControlller : MonoBehaviour
 {
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput; 
+    private float forwardInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,7 +15,11 @@ public class PlayerControlller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // We'll move the vehicle forward 
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on vertical input 
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
